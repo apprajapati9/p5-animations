4a1a20c3700125d00cafb9ecd46ce00136e123b0
Ajay P. Prajapati <apprajapati@hotmail.com>
2024-12-24 02:22:28 -0500

    snake in p5 updated with random spawn of food for snake and uprading snake length logic

--- snake.js
+++ snake.js
@@ -1,5 +1,12 @@
 
-class snake {
+class V2{
+    constructor(x,y){
+      this.x = x
+      this.y = y
+    }
+  }
+  
+  class snake {
     constructor(x,y) {
       this.x = x
       this.y = y
@@ -55,9 +62,22 @@ class snake {
   
   let longSnake = []
   
+  let direction = 3
+  
+  let fRate = 10
+  let updateFood = false
+  
+  function getRandomLocation() {
+    return random(lines/4, lines- (lines/4)).toFixed(0) 
+  }
+  
+  function getRandomFoodLocation() {
+    return random(0,lines).toFixed(0) 
+  }
+  
   function setup() {
     createCanvas(HEIGHT, WIDTH);
-    randomLocation = random(lines/4, lines- (lines/4)).toFixed(0) //keeping snake somehwere in the middle.
+    randomLocation = getRandomLocation() //keeping snake somehwere in the middle.
     sn.setLocation(randomLocation, randomLocation)
     
     let dir = random(0,2);
@@ -82,10 +102,12 @@ class snake {
     }
     
     console.log("snake..", longSnake)
+    sn.setLocation(getRandomLocation(), getRandomLocation())
   }
   
-  function updateSnakePosition(direction){
+  function updateSnakePosition(){
     
+    let extend = false
     let first = longSnake[0]
     first.x = Number(first.x)
     first.y = Number(first.y)
@@ -93,16 +115,50 @@ class snake {
     let temp = new snake(first.x, first.y)
       
       if(direction == 1){
-        first.x -= 1
+        if(first.x > 0) {
+          first.x -= 1
+        }else{
+          first.x = lines
+        }
+        
+        if(first.x == sn.x && first.y == sn.y){
+          extend = true
+        }
       }
       if(direction == 2){
-        first.x += 1
+      
+        if(first.x < lines){
+          first.x += 1
+        }else{
+          first.x = 0
+        }
+        if(first.x == sn.x && first.y == sn.y){
+          extend = true
+        }
       }
       if(direction == 3){
-        first.y -= 1
+        
+        if(first.y >= 0){
+          first.y -= 1
+        }else{
+          first.y = lines
+        }
+        
+        if(first.y == sn.y && first.x == sn.x){
+          extend = true
+        }
       }
       if(direction == 4){
-        first.y += 1
+        if(first.y <= lines){
+          first.y += 1
+        }else{
+          first.y = 0
+        }
+        
+        if(first.y == sn.y && first.x == sn.x){
+          extend = true
+        }
+        
       }
     
     for(let i = 1; i < longSnake.length; i++){
@@ -110,12 +166,28 @@ class snake {
         longSnake[i].setLocation(temp.x, temp.y)
         temp = current
     }
+    
+    if(extend){
+      updateFood = true
+      if(direction == 1 || direction == 2) {
+        longSnake.push(new snake(temp.x + 1, temp.y))
+      }
+      if(direction == 3 || direction == 4) {
+        longSnake.push(new snake(temp.x, temp.y + 1))
+      }
+    }
+    
+    if(updateFood){
+      sn.setLocation(getRandomFoodLocation(), getRandomFoodLocation())
+      updateFood = false
+    }
+    
   }
   
   
   function draw() {
     
-    frameRate(10)
+    frameRate(fRate)
     
     push()
     
@@ -126,6 +198,8 @@ class snake {
     scale(scaleFactor) // 600/60 = 10 lines. 
     // 600/10 = 60 lines.
   
+    
+      sn.drawSnake()
     
     strokeWeight(1/scaleFactor)
     for(let i = 0; i <= 60; i++) {
@@ -139,18 +213,23 @@ class snake {
     // Check for arrow key presses
     if (keyIsPressed) {
       if (keyCode === LEFT_ARROW) {
-        updateSnakePosition(1)
+        //updateSnakePosition(1)
+        direction = 1
       } else if (keyCode === RIGHT_ARROW) {
-        updateSnakePosition(2)
+        //updateSnakePosition(2)
+        direction = 2
       } else if (keyCode === UP_ARROW) {
-        updateSnakePosition(3)
+        //updateSnakePosition(3)
+        direction = 3
       } else if (keyCode === DOWN_ARROW) {
         //sn.moveSnake(4)
-        updateSnakePosition(4)
+        direction = 4
+        //updateSnakePosition(4)
       }
     }
     for(let i = 0; i < longSnake.length; i++){
       longSnake[i].drawSnake()
     }
-    
+    updateSnakePosition()
+
   }
